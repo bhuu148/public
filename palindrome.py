b593815c0fc3460c10c7007f4e15efeab93965f7
Bhuvana <bhuvaneshwari9042@gmail.com>
2024-09-11 14:33:50 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the number")
+t=a
+while(a>0):
+      r=temp%10
+      sum=sum*10+r
+      temp=temp/10
+if(a==r):
+      print("It is a palindrome")
+else:
+      print("It is not a palindrome")
